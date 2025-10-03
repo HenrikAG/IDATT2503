92423481200cc7f1573f9bd796e435b9b2b27a2b
Henrik <hengau@live.no>
2025-10-03 13:38:55 +0200

    Removed rust/target

--- assignment-5/task-2-rust/target/debug/deps/task_2_rust-8cc3c84e7d6f64a8.d
+++
@@ -1,5 +0,0 @@
-/home/henrik/Documents/NTNU/IDATT2503/assignments/assignment-5/task-2-rust/target/debug/deps/task_2_rust-8cc3c84e7d6f64a8.d: src/main.rs
-
-/home/henrik/Documents/NTNU/IDATT2503/assignments/assignment-5/task-2-rust/target/debug/deps/libtask_2_rust-8cc3c84e7d6f64a8.rmeta: src/main.rs
-
-src/main.rs:
